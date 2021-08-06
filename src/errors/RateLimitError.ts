f7ddb7910d19df8fa29818d0f05d719302907736
rui2015 <15705566+rui2015@users.noreply.github.com>
2021-08-06 17:09:30 +0100

    Add custom errors

---
+++ src/errors/RateLimitError.ts
@@ -0,0 +1,6 @@
+export default class RateLimitError extends Error {
+    constructor() {
+        super(`Rate limit exceeded. Please slow down before continuing.`)
+        this.name = 'RateLimitError';
+    }
+}
