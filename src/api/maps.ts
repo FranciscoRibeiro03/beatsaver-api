bfb7050cf55409110919934d64dc2d7828d1d7ec
rui2015 <15705566+rui2015@users.noreply.github.com>
2021-08-06 17:09:53 +0100

    Implement maps and search endpoints

---
+++ src/api/maps.ts
@@ -0,0 +1,51 @@
+import { AxiosInstance } from "axios";
+import RateLimitError from "../errors/RateLimitError";
+import SongNotFoundError from "../errors/SongNotFoundError";
+import { MapDetail } from "../models/MapDetail";
+import { SearchResponse } from "../models/SearchResponse";
+
+export async function getMapByID(axiosInstance: AxiosInstance, id: string): Promise<MapDetail> {
+    try {
+        const response = await axiosInstance.get(`/maps/id/${id}`);
+        return response.data as MapDetail;
+    } catch (err) {
+        const response = err.response;
+        if (response.status === 404) throw new SongNotFoundError('ID', id);
+        if (response.status === 429) throw new RateLimitError();
+        throw err;
+    }
+}
+
+export async function getMapByHash(axiosInstance: AxiosInstance, hash: string): Promise<MapDetail> {
+    try {
+        const response = await axiosInstance.get(`/maps/hash/${hash}`);
+        return response.data as MapDetail;
+    } catch (err) {
+        const response = err.response;
+        if (response.status === 404) throw new SongNotFoundError('hash', hash);
+        if (response.status === 429) throw new RateLimitError();
+        throw err;
+    }
+}
+
+export async function getMapsByUploader(axiosInstance: AxiosInstance, id: number, page: number = 0): Promise<SearchResponse> {
+    try {
+        const response = await axiosInstance.get(`/maps/uploader/${id}/${page}`);
+        return response.data as SearchResponse;
+    } catch (err) {
+        const response = err.response;
+        if (response.status === 429) throw new RateLimitError();
+        throw err;
+    }
+}
+
+export async function getLatestMaps(axiosInstance: AxiosInstance, automapper: boolean, before: string): Promise<SearchResponse> {
+    try {
+        const response = await axiosInstance.get(`/maps/latest?automapper=${String(automapper)}&before=${before}`);
+        return response.data as SearchResponse;
+    } catch (err) {
+        const response = err.response;
+        if (response.status === 429) throw new RateLimitError();
+        throw err;
+    }
+}
---
+++ src/api/search.ts
@@ -0,0 +1,43 @@
+import { AxiosInstance } from "axios";
+import RateLimitError from "../errors/RateLimitError";
+import { Instant } from "../models/Instant";
+import { SearchResponse } from "../models/SearchResponse";
+
+export enum SortOrder {
+    Latest = "Latest",
+    Relevance = "Relevance",
+    Rating = "Rating"
+}
+
+export interface SearchOptions {
+    sortOrder: SortOrder,
+    automapper?: boolean,
+    chroma?: boolean,
+    cinema?: boolean,
+    from?: Instant,
+    fullSpread?: boolean,
+    maxBpm?: number,
+    maxDuration?: number,
+    maxNps?: number,
+    maxRating?: number,
+    me?: boolean,
+    minBpm?: number,
+    minDuration?: number,
+    minNps?: number,
+    noodle?: boolean,
+    q?: string,
+    ranked?: boolean,
+    to?: Instant
+}
+
+
+export async function searchMaps(axiosInstance: AxiosInstance, searchOptions: SearchOptions = {sortOrder: SortOrder.Latest}, page: number = 0): Promise<SearchResponse> {
+    try {
+        const response = await axiosInstance.get(`/search/${page}`, {params: searchOptions})
+        return response.data as SearchResponse;
+    } catch (err) {
+        const response = err.response;
+        if (response.status === 429) throw new RateLimitError();
+        throw err;
+    }
+}
